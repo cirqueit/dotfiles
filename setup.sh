b15ee58d604b5021752449fcc3ef00a1fbbd7eb7
Joe Edwards <handbalancing@gmail.com>
2014-10-15 19:22:28 -0700

    added setup.sh script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p ~/.config/fish
+ln -s $(pwd)/config.fish ~/.config/fish/config.fish
+ln -s $(pwd)/.vimrc  ~/.vimrc
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
+if [ "$(uname)" == "Darwin" ]; then
+    ln -s $(pwd)/config.osx.fish ~/.config/fish/config.osx.fish
+    ln -s $(pwd)/.tmux.osx.conf ~/.tmux.osx.conf
+else
+    ln -s $(pwd)/config.linux.fish ~/.config/fish/config.linux.fish
+    ln -s $(pwd)/.tmux.linux.conf ~/.tmux.linux.conf
+fi
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
