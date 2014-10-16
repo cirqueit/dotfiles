2c34dc37f138820dc33c967bfb88d38e27cad835
Joe Edwards <handbalancing@gmail.com>
2014-10-16 14:48:32 -0700

    added linux specific ipython config

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 mkdir -p ~/.config/fish
-mkdir -p ~/.ipython/profile_default
 ln -s $(pwd)/config.fish ~/.config/fish/config.fish
 ln -s $(pwd)/.vimrc  ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 
 if [ "$(uname)" == "Darwin" ]; then
+    mkdir -p ~/.ipython/profile_default
+    ln -s $(pwd)/ipython_config.py ~/.ipython/profile_default/ipython_config.py
     ln -s $(pwd)/config.osx.fish ~/.config/fish/config.osx.fish
     ln -s $(pwd)/.tmux.osx.conf ~/.tmux.osx.conf
 else
+    mkdir -p ~.config/ipython/profile_default
+    ln -s $(pwd)/ipython_config.py ~/.config/ipython/profile_default/ipython_config.py
     ln -s $(pwd)/config.linux.fish ~/.config/fish/config.linux.fish
     ln -s $(pwd)/.tmux.linux.conf ~/.tmux.linux.conf
 fi
