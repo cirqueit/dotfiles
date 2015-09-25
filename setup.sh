a0ca69e678ab29aea4c8e9c92ba7ce05023fcc07
jedwards <handbalancing@gmail.com>
2015-09-24 19:00:08 -0700

    added curl z.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,9 @@ fi
 
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/autoload
+
 curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -flo ~/.z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 
 ln -sf ~/.vimrc  ~/.nvimrc
 ln -sf ~/.vim  ~/.nvim
